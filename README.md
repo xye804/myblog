4b9e17921f848e71c486bb42f0fce80e5770f3f5
xye804 <xye804@aucklanduni.ac.nz>
2024-04-01 23:49:41 +1300

    update

--- README.md
+++ README.md
@@ -12,28 +12,3 @@
 ![](https://img.shields.io/badge/font--awesome-✓-blue.svg)
 ![](https://img.shields.io/badge/devicon-✓-blue.svg)
 ![](https://img.shields.io/badge/gulp-✓-blue.svg)
-
-
-
-## Quick Setup
-
-1. Install Jekyll: `gem install jekyll bundler`
-2. Fork this repository and clone your fork
-3. Edit `_config.yml` to personalize your site
-
-# Color and Particle Customization
-
-- Color Customization
-  - Edit the `.sass` variables
-- Particle Customization
-  - Edit the json data in particle function in `app.js`
-  - Refer to `Particle.js` for help
-
-# Running locally
-
-In order to compile the assets and run `Jekyll` locally you need to follow those steps:
-
-1. Install Jekyll
-2. Run `bundle install`
-3. Run `bundle exec jekyll build`
-4. Start and http-server in the folder `_site`
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,48 @@
-const { src, dest, series } = require("gulp");
+const { src, dest, series, parallel, watch } = require("gulp");
 const csso = require("gulp-csso");
 const uglify = require("gulp-uglify");
 const concat = require("gulp-concat");
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
-const imagemin = import("gulp-imagemin");
+const imagemin = require("gulp-imagemin");
+const cp = require("child_process");
+const browserSync = require("browser-sync");
+
+const jekyllCommand = /^win/.test(process.platform) ? "jekyll.bat" : "bundle";
+
+/*
+ * Build the Jekyll Site
+ * runs a child process in node that runs the jekyll commands
+ */
+function jekyllBuildTask(done) {
+  return cp
+    .spawn(jekyllCommand, ["exec", "jekyll", "build"], { stdio: "inherit" })
+    .on("close", done);
+}
+
+/*
+ * Rebuild Jekyll & reload browserSync
+ */
+function jekyllRebuildTask(done) {
+  series(jekyllBuildTask, function () {
+    browserSync.reload();
+    done();
+  })();
+}
+
+/*
+ * Build the jekyll site and launch browser-sync
+ */
+function browserSyncTask(done) {
+  series(jekyllBuildTask, function () {
+    browserSync({
+      server: {
+        baseDir: "_site",
+      },
+    });
+    done();
+  })();
+}
 
 /*
  * Compile and minify sass
@@ -49,4 +87,21 @@ function jsTask() {
     .pipe(dest("assets/js/"));
 }
 
-exports.default = series(sassTask, fontsTask, jsTask);
+function watchTask() {
+  watch("src/styles/**/*.scss", series(sassTask, jekyllRebuildTask));
+  watch("src/js/**/*.js", series(jsTask, jekyllRebuildTask));
+  watch(
+    "src/fonts/**/*.{tff,woff,woff2}",
+    series(fontsTask, jekyllRebuildTask)
+  );
+  watch("src/img/**/*.{jpg,png,gif}", series(imageminTask, jekyllRebuildTask));
+  watch(["*html", "_includes/*html", "_layouts/*.html"], jekyllRebuildTask);
+}
+
+exports.default = parallel(
+  jsTask,
+  sassTask,
+  fontsTask,
+  browserSyncTask,
+  watchTask
+);
